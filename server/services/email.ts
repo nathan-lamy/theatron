1a0861fd3044bda4a34ba1efa16efa42b15dde5a
Rakox <rakoxwastaken@gmail.com>
2024-02-02 23:35:57 +0100

    ✨ Introduce confirm registration mail template

---
+++ server/services/email.ts
@@ -0,0 +1,38 @@
+import { User } from "@prisma/client";
+import Email from "email-templates";
+
+const email = new Email({
+  preview: {
+    open: {
+      app: "google-chrome",
+      wait: false,
+    },
+  },
+});
+
+email
+  .send({
+    template: "confirm-registration",
+    message: {
+      to: "elon@spacex.com",
+    },
+    locals: {
+      user: {
+        name: "Elon Musk",
+        email: "elon@spacex.com",
+        class: "SpaceX",
+      } as User,
+      event: {
+        id: 1,
+        name: "SpaceX Launch",
+        date: new Date(),
+        // TODO:
+        confirmBeforeDate: "31/12/2021",
+      },
+      link: "https://spacex.com",
+    },
+  })
+  .then((res) => {
+    console.log("res.originalMessage", res.originalMessage);
+  })
+  .catch(console.error);
