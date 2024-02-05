addcb3375aa74736e3c56169fb3fa7ee875bd6ac
Rakox <rakoxwastaken@gmail.com>
2024-02-05 19:05:35 +0100

    ✨ Introduce base mail service

--- server/services/email.ts
+++ server/services/email.ts
@@ -1,48 +1,54 @@
-import { User } from "@prisma/client";
+import { usersRepository } from "@/repositories/users";
+import { Event, User } from "@prisma/client";
 import Email from "email-templates";
 import nodemailer from "nodemailer";
 
 const email = new Email({
-  preview: {
+  preview: Bun.env.NODE_ENV === "development" && {
     open: {
       app: "google-chrome",
       wait: false,
     },
   },
+  send: Bun.env.NODE_ENV === "production",
   transport: nodemailer.createTransport({
-    host: "smtp.forwardemail.net",
+    host: Bun.env.SMTP_HOST,
     port: 465,
     secure: true,
     auth: {
-      user: "you@example.com",
-      pass: "****************************",
+      user: Bun.env.SMTP_USER,
+      pass: Bun.env.SMTP_PASSWORD,
     },
   }),
 });
 
-email
-  .send({
-    template: "event-reminder",
+export default async function sendEmail(
+  template: string,
+  {
+    user,
+    event,
+    additionalData,
+  }: {
+    user: number | User;
+    event: Event;
+    additionalData?: Record<string, any>;
+  }
+) {
+  if (typeof user === "number") {
+    user = (await usersRepository.getUserById(user))!;
+  }
+  await email.send({
+    template,
     message: {
-      to: "elon@spacex.com",
+      to: user.email,
     },
     locals: {
-      user: {
-        name: "Elon Musk",
-        email: "elon@spacex.com",
-        class: "SpaceX",
-      } as User,
-      event: {
-        id: 1,
-        name: "SpaceX Launch",
-        date: new Date(),
-        // TODO:
-        confirmBeforeDate: "31/12/2021",
-      },
-      link: "https://spacex.com",
+      user,
+      event,
+      ...additionalData,
+      functions: {},
     },
-  })
-  .then((res) => {
-    console.log("res.originalMessage", res.originalMessage);
-  })
-  .catch(console.error);
+  });
+}
+
+// TODO: Confirm before date
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,16 @@
 import runner from "@/services/runner";
 import app from "./app";
-import { prisma } from "./setup";
 
 // Connect to the database
-prisma.$connect().then(() => {
-  console.log("🦊 Database connected");
+import "./setup";
+console.log("🦊 Database connected");
 
-  // Start the job runner
-  runner();
+// Start the job runner
+runner();
 
-  // Start the server
-  app.listen(3333, () =>
-    console.log(
-      `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-    )
-  );
-});
+// Start the server
+app.listen(3333, () =>
+  console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  )
+);
