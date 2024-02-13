1431ab935b9a8eecc62fdc0a9faf75aa29e9e173
Nathan Lamy <nathan.lamycharrier@gmail.com>
2024-02-13 23:00:30 +0100

    💄 Update the register page UI

--- client/src/pages/confirm.tsx
+++ client/src/pages/confirm.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useNavigate, useSearchParams } from "react-router-dom";
+import Layout from "@/components/Layout";
 import {
   Card,
   CardContent,
@@ -11,7 +12,6 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import Footer from "@/components/Footer";
 import { client } from "@/lib/api";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { ERRORS, SUCCESS, redirect } from "@/lib/utils";
@@ -63,114 +63,111 @@ export default function ConfirmPage() {
   }
 
   return (
-    <main className="flex flex-col bg-gray-100 p-4 pt-0">
-      <div className="flex-1 flex justify-center items-center">
-        <div className="block">
-          <h1 className="text-center text-3xl font-bold text-gray-900 dark:text-gray-100 mb-12">
-            {isCanceling
-              ? "Désinscription théâtre 🎭"
-              : "Inscription au théâtre 🎭"}
-          </h1>
-          <Card className="w-full max-w-md mx-auto">
-            <CardHeader>
-              <CardTitle className="text-2xl">{event.title}</CardTitle>
-              {event.details && (
-                <CardTitle className="font-medium">{event.details}</CardTitle>
+    <Layout>
+      <div className="block">
+        <h1 className="text-center text-3xl font-bold text-gray-900 dark:text-gray-100 mb-12">
+          {isCanceling
+            ? "Désinscription théâtre 🎭"
+            : "Inscription au théâtre 🎭"}
+        </h1>
+        <Card className="w-full max-w-md mx-auto">
+          <CardHeader>
+            <CardTitle className="text-2xl">{event.title}</CardTitle>
+            {event.details && (
+              <CardTitle className="font-medium">{event.details}</CardTitle>
+            )}
+            <CardDescription>
+              {isCanceling ? (
+                <>
+                  Vous êtes sur le point de vous <b>désinscrire</b> de ce
+                  spectacle. Renseignez le motif de votre désinscription et
+                  cliquez sur le bouton ci-dessous pour confirmer.
+                </>
+              ) : (
+                <>
+                  Vous êtes pré-inscrit à ce spectacle.
+                  <br />
+                  Veuillez <b>confirmer avant le {confirmBeforeDate}</b> pour
+                  valider définitivement, faute de quoi votre place sera
+                  réattribuée.
+                </>
               )}
-              <CardDescription>
-                {isCanceling ? (
-                  <>
-                    Vous êtes sur le point de vous <b>désinscrire</b> de ce
-                    spectacle. Renseignez le motif de votre désinscription et
-                    cliquez sur le bouton ci-dessous pour confirmer.
-                  </>
-                ) : (
-                  <>
-                    Vous êtes pré-inscrit à ce spectacle.
-                    <br />
-                    Veuillez <b>confirmer avant le {confirmBeforeDate}</b> pour
-                    valider définitivement, faute de quoi votre place sera
-                    réattribuée.
-                  </>
-                )}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className="space-y-4">
-                {isCanceling ? (
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-4">
+              {isCanceling ? (
+                <div className="space-y-2">
+                  <Label htmlFor="reason">Motif de la désinscription</Label>
+                  <Textarea
+                    id="reason"
+                    placeholder="Je ne peux plus venir..."
+                    onChange={(e) => setReason(e.target.value)}
+                  />
+                </div>
+              ) : (
+                <>
                   <div className="space-y-2">
-                    <Label htmlFor="reason">Motif de la désinscription</Label>
-                    <Textarea
-                      id="reason"
-                      placeholder="Je ne peux plus venir..."
-                      onChange={(e) => setReason(e.target.value)}
+                    <Label htmlFor="name">Nom</Label>
+                    <Input
+                      id="name"
+                      placeholder="Monsieur Saly"
+                      value={user.name}
+                      disabled
                     />
                   </div>
-                ) : (
-                  <>
-                    <div className="space-y-2">
-                      <Label htmlFor="name">Nom</Label>
-                      <Input
-                        id="name"
-                        placeholder="Monsieur Saly"
-                        value={user.name}
-                        disabled
-                      />
-                    </div>
-                    <div className="space-y-2">
-                      <Label htmlFor="email">Adresse mail</Label>
-                      <Input
-                        id="email"
-                        type="email"
-                        placeholder="adrien.saly@ac-nice.fr"
-                        value={user.email}
-                        disabled
-                      />
-                    </div>
-                  </>
-                )}
-                <div className="flex space-x-4 pt-4">
-                  {isCanceling ? (
-                    <Button
-                      className="flex-1"
-                      variant="outline"
-                      onClick={() => !user.onWaitList && setIsCanceling(false)}
-                      disabled={isLoading || user.onWaitList}
-                    >
-                      ANNULER
-                    </Button>
-                  ) : (
-                    <Button
-                      className="flex-1"
-                      onClick={() => {
-                        void confirm();
-                      }}
-                      disabled={isLoading}
-                    >
-                      {isLoading && (
-                        <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-                      )}
-                      CONFIRMER
-                    </Button>
-                  )}
+                  <div className="space-y-2">
+                    <Label htmlFor="email">Adresse mail</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="adrien.saly@ac-nice.fr"
+                      value={user.email}
+                      disabled
+                    />
+                  </div>
+                </>
+              )}
+              <div className="flex space-x-4 pt-4">
+                {isCanceling ? (
                   <Button
                     className="flex-1"
-                    variant="destructive"
-                    onClick={isCanceling ? cancel : () => setIsCanceling(true)}
+                    variant="outline"
+                    onClick={() => !user.onWaitList && setIsCanceling(false)}
+                    disabled={isLoading || user.onWaitList}
+                  >
+                    ANNULER
+                  </Button>
+                ) : (
+                  <Button
+                    className="flex-1"
+                    onClick={() => {
+                      void confirm();
+                    }}
                     disabled={isLoading}
                   >
-                    {isLoading && isCanceling && (
+                    {isLoading && (
                       <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
                     )}
-                    SE DÉSINSCRIRE
+                    CONFIRMER
                   </Button>
-                </div>
+                )}
+                <Button
+                  className="flex-1"
+                  variant="destructive"
+                  onClick={isCanceling ? cancel : () => setIsCanceling(true)}
+                  disabled={isLoading}
+                >
+                  {isLoading && isCanceling && (
+                    <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  SE DÉSINSCRIRE
+                </Button>
               </div>
-            </CardContent>
-          </Card>
-        </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
-      <Footer />
-    </main>
+    </Layout>
   );
 }
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -11,50 +11,134 @@ import {
 } from "@/components/ui/select";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
+import Layout from "@/components/Layout";
+import { useState } from "react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
 
 export default function Register() {
+  // Create state for the current step
+  const [currentStep, setCurrentStep] = useState("step1");
+  const [error, setError] = useState("");
+  // Create state for the form inputs (email, first name, last name, class level, class number)
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [classLevel, setClassLevel] = useState("");
+  const [classNumber, setClassNumber] = useState("");
+  // Create state for the selected events
+  const [selectedEvents, setSelectedEvents] = useState([] as string[]);
+  // Create state for the order of preference
+  const [preferenceOrder, setPreferenceOrder] = useState([]);
+
+  function handleSubmit() {
+    // Handle form submission
+    let handler: (() => string | undefined) | undefined;
+    if (currentStep === "step1") handler = handleFirstStep;
+    if (currentStep === "step2") handler = handleSecondStep;
+    if (currentStep === "step3") handler = handleThirdStep;
+    if (!handler) return;
+    // If there is an error, display it
+    const error = handler();
+    if (error) setError(error);
+    else setError("");
+  }
+
+  function handleFirstStep() {
+    if (!email) {
+      return "Merci de renseigner votre adresse mail.";
+    }
+    if (!firstName || !lastName) {
+      return "Merci de renseigner votre prénom et votre nom.";
+    }
+    if (!classLevel || !classNumber) {
+      return "Merci de renseigner votre niveau et votre classe.";
+    }
+    // Handle the first step of the form
+    setCurrentStep("step2");
+  }
+
+  function handleSecondStep() {
+    // Save the selected events
+    const selectedIds = [] as string[];
+    const checkboxes = document.querySelectorAll(
+      'button[role="checkbox"][id^="event"][data-state="checked"]'
+    );
+    if (!checkboxes.length) {
+      return "Merci de sélectionner au moins un spectacle.";
+    }
+    checkboxes.forEach((checkbox) =>
+      selectedIds.push(checkbox.id.replace("event", ""))
+    );
+    setSelectedEvents(selectedIds);
+    // Handle the second step of the form
+    setCurrentStep("step3");
+  }
+
   return (
-    <>
+    <Layout>
       <div className="mx-auto max-w-[600px] space-y-6">
         <div className="space-y-2 text-center">
-          <h1 className="text-3xl font-bold">Event Registration</h1>
+          <h1 className="text-3xl font-bold">Inscription au théâtre 🎭</h1>
           <p className="text-gray-500 dark:text-gray-400">
-            Register for your favorite events in three simple steps.
+            Pré-inscrivez-vous pour assister à un ou même plusieurs spectacles{" "}
+            <b>gratuitement</b> !
           </p>
         </div>
-        <Tabs className="w-full" value="step1">
-          <TabsList className="flex justify-between">
+        <Tabs className="w-full" value={currentStep}>
+          <TabsList className="flex">
             <TabsTrigger value="step1">
-              Step 1: Identity Information
+              Étape 1 : Informations personnelles
             </TabsTrigger>
-            <TabsTrigger value="step2">Step 2: Event Selection</TabsTrigger>
-            <TabsTrigger value="step3">Step 3: Priority Sorting</TabsTrigger>
+            <TabsTrigger value="step2">
+              Étape 2 : Sélection des spectacles
+            </TabsTrigger>
+            <TabsTrigger value="step3">
+              Étape 3 : Ordre de préférence
+            </TabsTrigger>
           </TabsList>
           <TabsContent value="step1">
             <div className="space-y-4">
               <div className="space-y-2">
-                <Label htmlFor="email">Email</Label>
+                <Label htmlFor="email">Adresse mail</Label>
                 <Input
                   id="email"
-                  placeholder="m@example.com"
+                  onChange={(e) => setEmail(e.target.value)}
+                  defaultValue={email}
+                  placeholder="adrien.saly@ac-nice.fr"
                   required
                   type="email"
                 />
               </div>
               <div className="grid grid-cols-2 gap-4">
                 <div className="space-y-2">
-                  <Label htmlFor="first-name">First name</Label>
-                  <Input id="first-name" placeholder="John" required />
+                  <Label htmlFor="first-name">Prénom</Label>
+                  <Input
+                    id="first-name"
+                    onChange={(e) => setFirstName(e.target.value)}
+                    defaultValue={firstName}
+                    placeholder="Adrien"
+                    required
+                  />
                 </div>
                 <div className="space-y-2">
-                  <Label htmlFor="last-name">Last name</Label>
-                  <Input id="last-name" placeholder="Doe" required />
+                  <Label htmlFor="last-name">Nom</Label>
+                  <Input
+                    id="last-name"
+                    onChange={(e) => setLastName(e.target.value)}
+                    defaultValue={lastName}
+                    placeholder="SALY"
+                    required
+                  />
                 </div>
               </div>
               <div className="grid grid-cols-2 gap-4">
                 <div className="space-y-2">
-                  <Label htmlFor="class-level">Class Level</Label>
-                  <Select required>
+                  <Label htmlFor="class-level">Niveau</Label>
+                  <Select
+                    onValueChange={(val) => setClassLevel(val)}
+                    defaultValue={classLevel}
+                    required
+                  >
                     <SelectTrigger>
                       <SelectValue placeholder="Choisissez votre classe" />
                     </SelectTrigger>
@@ -68,9 +152,13 @@ export default function Register() {
                   </Select>
                 </div>
                 <div className="space-y-2">
-                  <Label htmlFor="class-number">Class Number</Label>
+                  <Label htmlFor="class-number">
+                    Classe (lettre ou numéro)
+                  </Label>
                   <Input
                     id="class-number"
+                    onChange={(e) => setClassNumber(e.target.value)}
+                    defaultValue={classNumber}
                     maxLength={1}
                     placeholder="A"
                     required
@@ -82,29 +170,33 @@ export default function Register() {
           <TabsContent value="step2">
             <div className="space-y-4">
               <div className="grid grid-cols-2 gap-4">
-                <div className="space-y-2">
-                  <Checkbox id="event1" name="event" />
-                  <Label className="text-sm font-normal" htmlFor="event1">
-                    Event 1 - Date: 12/12/2024
-                  </Label>
+                <div className="flex space-x-2">
+                  <Checkbox
+                    id="event1"
+                    defaultChecked={selectedEvents.includes("1")}
+                  />
+                  <Label htmlFor="event1">Event 1 - Date: 12/12/2024</Label>
                 </div>
-                <div className="space-y-2">
-                  <Checkbox id="event2" name="event" />
-                  <Label className="text-sm font-normal" htmlFor="event2">
-                    Event 2 - Date: 15/12/2024
-                  </Label>
+                <div className="flex space-x-2">
+                  <Checkbox
+                    id="event2"
+                    defaultChecked={selectedEvents.includes("2")}
+                  />
+                  <Label htmlFor="event2">Event 2 - Date: 15/12/2024</Label>
                 </div>
-                <div className="space-y-2">
-                  <Checkbox id="event3" name="event" />
-                  <Label className="text-sm font-normal" htmlFor="event3">
-                    Event 3 - Date: 20/12/2024
-                  </Label>
+                <div className="flex space-x-2">
+                  <Checkbox
+                    id="event3"
+                    defaultChecked={selectedEvents.includes("3")}
+                  />
+                  <Label htmlFor="event3">Event 3 - Date: 20/12/2024</Label>
                 </div>
-                <div className="space-y-2">
-                  <Checkbox id="event4" name="event" />
-                  <Label className="text-sm font-normal" htmlFor="event4">
-                    Event 4 - Date: 25/12/2024
-                  </Label>
+                <div className="flex space-x-2">
+                  <Checkbox
+                    id="event4"
+                    defaultChecked={selectedEvents.includes("4")}
+                  />
+                  <Label htmlFor="event4">Event 4 - Date: 25/12/2024</Label>
                 </div>
               </div>
             </div>
@@ -112,29 +204,49 @@ export default function Register() {
           <TabsContent value="step3">
             <div className="space-y-4">
               <p className="text-gray-500 dark:text-gray-400">
-                Drag and drop to sort your selected events by priority.
+                Classez les spectacles par ordre de préférence.
+                <br />
+                Le premier, tout en haut de la liste, est votre spectacle
+                préféré.
               </p>
               <div className="grid grid-cols-1 gap-4">
-                <div className="bg-gray-100 dark:bg-gray-800 p-2 rounded-md">
+                <div className="bg-slate-200 dark:bg-gray-800 p-2 rounded-md">
                   Event 1 - Date: 12/12/2024
                 </div>
-                <div className="bg-gray-100 dark:bg-gray-800 p-2 rounded-md">
+                <div className="bg-slate-200 dark:bg-gray-800 p-2 rounded-md">
                   Event 2 - Date: 15/12/2024
                 </div>
-                <div className="bg-gray-100 dark:bg-gray-800 p-2 rounded-md">
+                <div className="bg-slate-200 dark:bg-gray-800 p-2 rounded-md">
                   Event 3 - Date: 20/12/2024
                 </div>
-                <div className="bg-gray-100 dark:bg-gray-800 p-2 rounded-md">
+                <div className="bg-slate-200 dark:bg-gray-800 p-2 rounded-md">
                   Event 4 - Date: 25/12/2024
                 </div>
               </div>
             </div>
           </TabsContent>
         </Tabs>
-        <Button className="w-full" type="submit">
-          Register
-        </Button>
+        <div className="text-red-500 dark:text-red-400">{error || ""}</div>
+        <div className="flex space-x-4">
+          {currentStep === "step1" ? null : (
+            <Button
+              className="w-full"
+              type="button"
+              onClick={() =>
+                setCurrentStep("step" + (Number(currentStep[4]) - 1))
+              }
+              variant="secondary"
+            >
+              <ArrowLeftIcon className="mr-2 h-4 w-4" />
+              Retour
+            </Button>
+          )}
+          <Button className="w-full" type="button" onClick={handleSubmit}>
+            Poursuivre l&apos;inscription
+            <ArrowRightIcon className="mr-2 h-4 w-4" />
+          </Button>
+        </div>
       </div>
-    </>
+    </Layout>
   );
 }
